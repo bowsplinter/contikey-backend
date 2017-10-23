d2b35dd20cccfa72b9a5f87d4e5bc87ee003edc4
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-10-23 22:54:00 +0800

    Change database to mysql

--- dboard/settings.py
+++ dboard/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dboard',
+        'USER': 'root',
+        'PASSWORD': 'vivekkalyan',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
