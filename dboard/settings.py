1817ad03942cae0af06e507e45317a62cd1c854f
pammelang <poppyfights@gmail.com>
2017-11-05 13:03:27 +0800

    connected to AWS RDS and elastic beanstalk

--- dboard/settings.py
+++ dboard/settings.py
@@ -73,16 +73,17 @@ WSGI_APPLICATION = 'dboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dboard',
-        'USER': 'root',
-        'PASSWORD': 'vivekkalyan',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 
 
 # Password validation
