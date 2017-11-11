ee43304960f649fe3e5e03075189d76f0b9b4a6b
Teo Si-Yan <teosiyan@hotmail.com>
2017-11-11 19:42:49 +0800

    Set up settings for local development

---
+++ .env.py
@@ -0,0 +1,9 @@
+# environment variables for local development
+
+settings = {
+    'RDS_DB_NAME': '',
+    'RDS_USERNAME': '',
+    'RDS_PASSWORD': '',
+    'RDS_HOSTNAME': '',
+    'RDS_PORT': '',
+}
