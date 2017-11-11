1dbb00071b1a32f72af981c1ef53169b344b2869
Teo Si-Yan <teosiyan@hotmail.com>
2017-11-11 19:42:49 +0800

    Set up settings for local development

---
+++ dboard/settings_local.py
@@ -0,0 +1,13 @@
+from dboard.settings import *
+from env import settings
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': settings['RDS_DB_NAME'],
+        'USER': settings['RDS_USERNAME'],
+        'PASSWORD': settings['RDS_PASSWORD'],
+        'HOST': settings['RDS_HOSTNAME'],
+        'PORT': settings['RDS_PORT'],
+    }
+}
