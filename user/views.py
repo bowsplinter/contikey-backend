d41780da8849fea3c81baeaa2ad8b45c675bddc6
Teo Si-Yan <teosiyan@hotmail.com>
2017-11-20 16:36:31 +0800

    Basic functionality for user API

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.db import connection
 
-# Create your views here.
+def profile(request, user_id = 'me'):
+  if user_id == 'me':
+    user_id = 1 #.should get user's user_id from session key
+
+  with connection.cursor() as cursor:
+    cursor.execute("SELECT * FROM user WHERE user_id = %s", [user_id])
+    columns = [col[0] for col in cursor.description]
+    row = cursor.fetchone()
+    return JsonResponse({'user': dict(zip(columns, row))})
