d885b06e0364cc6dd834e2fdc92433a7540f6b4c
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-11-20 18:08:57 +0800

    Add function to add notification
    Requires user_id to exist

--- notification/sql.py
+++ notification/sql.py
@@ -8,3 +8,15 @@ def get_notification_detail(self):
         """)
         data = cursor.fetchall()
     return data
+
+def add_notification(self, data):
+    text = data['text']
+    url = data['url']
+    user_id = data['user_id']
+
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT INTO notification (text, url, user_id)
+            VALUES (%s, %s, %s)
+            """, [text, url, user_id])
+    return 0
