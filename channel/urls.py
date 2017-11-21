a58a2de64271486909adf399b37e43048b227836
Joe <tyclian@hotmail.com>
2017-11-22 01:23:56 +0800

    wip pending further discussion on endpoint naming

--- channel/urls.py
+++ channel/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     # /explore
     # /feed
-    # /channel/ - defaults to view channel 1
+    #GET /channel/ - defaults to view channel 1
     url(r'^$', views.channel, name='channels'),
-    # /channel/<channel_id>/
+    #GET /channel/<channel_id>/
     url(r'^(?P<channel_id>[0-9]+)/$', views.channel, name='channels'),
-    # /channel/new
+    #POST /channel/new
     url(r'^new/$', views.new, name='new_channel'),
-    # /channel/follow
+    #POST /channel/follow
     url(r'^follow/$', views.follow, name='follow_channel'),
 ]	
