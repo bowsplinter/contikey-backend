60f9cd86df8e8f3247ba46b6281ffaa0830ba2c1
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-11-29 23:12:51 +0800

    Fix user id and type user id

---
+++ comment/migrations/0004_comment_notification_trigger.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('comment', '0001_create_comment_table'),
+        ('comment', '0002_comment_notification_trigger'),
+        ('comment', '0003_delete_comment_notification_trigger'),
+        ('notification', '0003_add_type_user_notification_table')
+    ]
+
+    operations = [
+        migrations.RunSQL("""
+            CREATE TRIGGER comment_notification AFTER INSERT ON comment
+            FOR EACH ROW
+            INSERT INTO notification (type, article_id, user_id, is_read, type_user_id)
+            VALUES ('comment', NEW.article_id, (
+                 SELECT user_id from channel WHERE channel_id IN (
+-                    SELECT channel_id FROM article WHERE article_id = NEW.article_id)))
++                    SELECT channel_id FROM article WHERE article_id = NEW.article_id)), false, NEW.user_id)
+        """,
+        "DROP TRIGGER comment_notification"
+      )
+    ]
