e5c8d56d4ebbfc3a12bd943a392379b3aee7fc02
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-11-29 21:51:54 +0800

    Change notification table to include is_read and article_id

--- notification/migrations/0002_modify_notification_table.py
+++ notification/migrations/0002_modify_notification_table.py
@@ -7,23 +7,37 @@ class Migration(migrations.Migration):
 
     dependencies = [
       ('user', '0001_create_user_table'),
+      ('article', '0001_create_article_table'),
       ('notification', '0001_create_notification_table')
     ]
 
     operations = [
         migrations.RunSQL("""
             ALTER TABLE notification
-            DROP COLUMN text
-            DROP COLUMN url
-            ADD COLUMN type VARCHAR(20)
-            ADD COLUMN type_id INT NOT NULL
+            DROP COLUMN text,
+            DROP COLUMN url,
+            ADD COLUMN type VARCHAR(20),
+            ADD COLUMN is_read BOOLEAN,
+            ADD COLUMN article_id INT
+        ;""",
+        """
+            ALTER TABLE notification
+            DROP COLUMN type,
+            DROP COLUMN article_id,
+            DROP COLUMN is_read,
+            ADD COLUMN text VARCHAR(500),
+            ADD COLUMN url VARCHAR(500)
+        """
+        ),
+        migrations.RunSQL("""
+            ALTER TABLE notification
+            ADD CONSTRAINT articleFK FOREIGN KEY(article_id) REFERENCES article(article_id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE;
         """,
         """
             ALTER TABLE notification
-            DROP COLUMN type
-            DROP COLUMN type_id
-            ADD COLUMN text VARCHAR(500)
-            ADD COLUMN url VARCHAR(500)
+            DROP CONSTRAINT articleFK
         """
         )
     ]
