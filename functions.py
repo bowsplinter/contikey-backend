4ed581bdaee66bed623be3d9e2c0ba33f7171c88
Teo Si-Yan <teosiyan@hotmail.com>
2017-12-04 21:12:24 +0800

    Add dictfetchone function

--- functions.py
+++ functions.py
@@ -6,6 +6,10 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
+def dictfetchone(cursor):
+    "Return one row from a cursor as a dict"
+    return dictfetchall(cursor)[0]
+
 def listfetchall(cursor):
     "Return list of the first entry in each row from a cursor (eg. for getting list of ids)"
     return [row[0] for row in cursor.fetchall()]
