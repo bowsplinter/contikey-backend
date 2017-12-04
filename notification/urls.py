7101d6bc4aad5e6c2a34b2783ecd922c8352d2c0
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-12-04 21:23:05 +0800

    Add api to read notification

--- notification/urls.py
+++ notification/urls.py
@@ -3,4 +3,5 @@ from notification import views
 
 urlpatterns = [
     url(r'^$', views.notification_list.as_view(), name='notification-list'),
+    url(r'^(?P<notification_id>[0-9]+)/$', views.notification_detail.as_view(), name='notifcation-detail')
 ]
