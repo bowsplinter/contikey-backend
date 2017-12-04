c39b03a995d1f73a08e5ee5ac76ae823d928a105
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-12-04 15:41:43 +0800

    Change notification to require cookie

--- notification/views.py
+++ notification/views.py
@@ -8,5 +8,9 @@ class notification_list(APIView):
     """
     List all snippets, or create a new snippet.
     """
-    def get(self, request, user_id, format=None):
+    def get(self, request, format=None):
+        try: # get user's user_id from session key
+            user_id = request.session['user_id']
+        except:
+            return Response({'error': 'no existing session or session expired'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(get_notification_list(user_id), status=status.HTTP_200_OK)
