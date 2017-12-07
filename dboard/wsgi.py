744abfe259ca7cc280a224bfb3669db0d1d71e99
pammelang <poppyfights@gmail.com>
2017-12-08 04:29:47 +0800

    Added unfollow trigger for notifications

--- dboard/wsgi.py
+++ dboard/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dboard.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dboard.settings.local")
 
 application = get_wsgi_application()
