6a9a91b7bed684cf5de5260461873c1f200d6145
Joe <tyclian@hotmail.com>
2017-12-08 14:07:47 +0800

    channellist_get articles & userid_get_articles orderby created_at desc, using tabs

--- functionssql.py
+++ functionssql.py
@@ -4,54 +4,54 @@ from django.db import connection
 from functions import dictfetchall, dictfetchone
 
 def channellist_get_articles(channelList):
-    with connection.cursor() as cursor:
-        for channel in channelList:
-            cursor.execute("""
-                SELECT * FROM article WHERE channel_id = %s
-                LIMIT 4
-                """,
-                [channel['channel_id']])
-            channel['articles'] = dictfetchall(cursor)
-    return channelList
+	with connection.cursor() as cursor:
+		for channel in channelList:
+			cursor.execute("""
+				SELECT * FROM article WHERE channel_id = %s
+				ORDER BY created_at DESC LIMIT 4
+				""",
+				[channel['channel_id']])
+			channel['articles'] = dictfetchall(cursor)
+	return channelList
 
 def channellist_get_user(channelList):
-    with connection.cursor() as cursor:
-        for channel in channelList:
-            cursor.execute("""
-                SELECT * FROM user WHERE user_id = %s
-                """,
-                [channel['user_id']])
-            channel['user'] = dictfetchone(cursor)
-    return channelList
+	with connection.cursor() as cursor:
+		for channel in channelList:
+			cursor.execute("""
+				SELECT * FROM user WHERE user_id = %s
+				""",
+				[channel['user_id']])
+			channel['user'] = dictfetchone(cursor)
+	return channelList
 
 def articlelist_get_channel(articleList):
-    with connection.cursor() as cursor:
-        for article in articleList:
-            if not article['channel_id']:
-                article['channel'] = None
-            else:
-                cursor.execute("SELECT * FROM channel WHERE channel_id = %s",
-                    [article['channel_id']])
-                article['channel'] = dictfetchone(cursor)
-    return articleList
+	with connection.cursor() as cursor:
+		for article in articleList:
+			if not article['channel_id']:
+				article['channel'] = None
+			else:
+				cursor.execute("SELECT * FROM channel WHERE channel_id = %s",
+					[article['channel_id']])
+				article['channel'] = dictfetchone(cursor)
+	return articleList
 
 def articlelist_get_user(articleList):
-    # use this with the result of articlelist_get_channel
-    with connection.cursor() as cursor:
-        for article in articleList:
-            if not article['channel'] or not article['channel']['user_id']:
-                article['user'] = None
-            else:
-                cursor.execute("SELECT * FROM user WHERE user_id = %s",
-                    [article['channel']['user_id']])
-                article['user'] = dictfetchone(cursor)
-    return articleList
+	# use this with the result of articlelist_get_channel
+	with connection.cursor() as cursor:
+		for article in articleList:
+			if not article['channel'] or not article['channel']['user_id']:
+				article['user'] = None
+			else:
+				cursor.execute("SELECT * FROM user WHERE user_id = %s",
+					[article['channel']['user_id']])
+				article['user'] = dictfetchone(cursor)
+	return articleList
 
 #Gets if current user is subscribed to given list of channels 
 def channel_get_subscribed(channelList, user_id):
-    with connection.cursor() as cursor:
-        for channel in channelList:
-            cursor.execute("SELECT exists(SELECT 1 FROM user_follows_channel WHERE user_id = %s AND channel_id = %s) subscribed", [user_id, channel['channel_id']])
-            data = dictfetchone(cursor)
-            channel['subscribed'] = True if data['subscribed'] is 1 else False;
-    return channelList  
+	with connection.cursor() as cursor:
+		for channel in channelList:
+			cursor.execute("SELECT exists(SELECT 1 FROM user_follows_channel WHERE user_id = %s AND channel_id = %s) subscribed", [user_id, channel['channel_id']])
+			data = dictfetchone(cursor)
+			channel['subscribed'] = True if data['subscribed'] is 1 else False;
+	return channelList  
--- user/sql.py
+++ user/sql.py
@@ -3,84 +3,84 @@ from functions import dictfetchall, dictfetchone
 import functionssql as fs
 
 def userid_get_user(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM user WHERE user_id = %s", [user_id])
-        return dictfetchone(cursor)
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM user WHERE user_id = %s", [user_id])
+		return dictfetchone(cursor)
 
 def facebookid_get_user(facebook_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM user WHERE facebook_id = %s", [facebook_id])
-        return dictfetchall(cursor)
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM user WHERE facebook_id = %s", [facebook_id])
+		return dictfetchall(cursor)
 
 def userid_get_channels(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM channel WHERE user_id = %s", [user_id])
-        channellist = dictfetchall(cursor)
-    return fs.channellist_get_articles(channellist)
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM channel WHERE user_id = %s", [user_id])
+		channellist = dictfetchall(cursor)
+	return fs.channellist_get_articles(channellist)
 
 def userid_get_articles(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT * FROM article WHERE channel_id IN (
-                SELECT channel_id FROM channel WHERE user_id = %s
-            )""",
-            [user_id])
-        articles = dictfetchall(cursor)
-    return fs.articlelist_get_channel(articles)
+	with connection.cursor() as cursor:
+		cursor.execute("""
+			SELECT * FROM article WHERE channel_id IN (
+				SELECT channel_id FROM channel WHERE user_id = %s
+			) ORDER BY created_at DESC""",
+			[user_id])
+		articles = dictfetchall(cursor)
+	return fs.articlelist_get_channel(articles)
 
 def userid_get_tagids(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT tag_id FROM user_follows_tag WHERE user_id = %s", [user_id])
-        return dictfetchall(cursor)
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT tag_id FROM user_follows_tag WHERE user_id = %s", [user_id])
+		return dictfetchall(cursor)
 
 def userid_get_friends(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT * FROM user WHERE user_id IN (
-                SELECT friend_id FROM user_friends WHERE user_id = %s
-                UNION
-                SELECT user_id FROM user_friends WHERE friend_id = %s
-            )""",
-            [user_id, user_id])
-        return dictfetchall(cursor)
+	with connection.cursor() as cursor:
+		cursor.execute("""
+			SELECT * FROM user WHERE user_id IN (
+				SELECT friend_id FROM user_friends WHERE user_id = %s
+				UNION
+				SELECT user_id FROM user_friends WHERE friend_id = %s
+			)""",
+			[user_id, user_id])
+		return dictfetchall(cursor)
 
 def userid_get_following(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT * FROM channel WHERE channel_id IN (
-            SELECT channel_id FROM user_follows_channel WHERE user_id = %s
-            )""",
-            [user_id])
-        channels = dictfetchall(cursor)
-        channels = fs.channellist_get_articles(channels)
-        channels = fs.channellist_get_user(channels)
-        channels = fs.channel_get_subscribed(channels,user_id)
-    return channels
+	with connection.cursor() as cursor:
+		cursor.execute("""
+			SELECT * FROM channel WHERE channel_id IN (
+			SELECT channel_id FROM user_follows_channel WHERE user_id = %s
+			)""",
+			[user_id])
+		channels = dictfetchall(cursor)
+		channels = fs.channellist_get_articles(channels)
+		channels = fs.channellist_get_user(channels)
+		channels = fs.channel_get_subscribed(channels,user_id)
+	return channels
 
 def insert_user(data):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            INSERT INTO user (facebook_id, name, email, photo)
-            VALUES (%s, %s, %s, %s)""",
-            [data.get('facebook_id'), data.get('name'), data.get('email'), data.get('photo')])
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        user_id = cursor.fetchone()[0]
-        return user_id
+	with connection.cursor() as cursor:
+		cursor.execute("""
+			INSERT INTO user (facebook_id, name, email, photo)
+			VALUES (%s, %s, %s, %s)""",
+			[data.get('facebook_id'), data.get('name'), data.get('email'), data.get('photo')])
+		cursor.execute("SELECT LAST_INSERT_ID()")
+		user_id = cursor.fetchone()[0]
+		return user_id
 
 def insert_user_friends(user_id, friend_fbid_list):
-    for friend_fbid in friend_fbid_list:
-        friend_user = facebookid_get_user(friend_fbid)
-        if not friend_user: # for development. this shouldn't be needed in production
-            continue
-        friend_userid = friend_user[0]['user_id']
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                INSERT INTO user_friends (user_id, friend_id)
-                VALUES (%s, %s)""",
-                [user_id, friend_userid])
-    return True
+	for friend_fbid in friend_fbid_list:
+		friend_user = facebookid_get_user(friend_fbid)
+		if not friend_user: # for development. this shouldn't be needed in production
+			continue
+		friend_userid = friend_user[0]['user_id']
+		with connection.cursor() as cursor:
+			cursor.execute("""
+				INSERT INTO user_friends (user_id, friend_id)
+				VALUES (%s, %s)""",
+				[user_id, friend_userid])
+	return True
 
 def delete_user(user_id):
-    with connection.cursor() as cursor:
-        d = cursor.execute("DELETE FROM user WHERE user_id = %s", [user_id])
-        return d
+	with connection.cursor() as cursor:
+		d = cursor.execute("DELETE FROM user WHERE user_id = %s", [user_id])
+		return d
